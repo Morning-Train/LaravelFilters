99595ea0cc6a49f79f7630cd6b9930eeac5d371c
Bjarne <bb@morningtrain.dk>
2020-11-02 12:48:07 +0100

    Implemented draft of PeriodFilter

---
+++ src/Filters/PeriodFilter.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace MorningTrain\Laravel\Filters\Filters;
+
+use Carbon\Carbon;
+
+class PeriodFilter extends Filter
+{
+
+    public function __construct($key, $scope)
+    {
+
+        $this->scope($scope);
+
+        $this->when($key, function ($q, $value) {
+
+
+            $from = data_get($value, 'from');
+            $to = data_get($value, 'to');
+
+            if ($from && $to) {
+
+                if($this->cast_to_carbon === true) {
+                    if(is_string($from)) {
+                        $from = Carbon::createFromFormat($this->input_format, $from);
+                    }
+
+                    if(is_string($to)) {
+                        $to = Carbon::createFromFormat($this->input_format, $to);
+                    }
+                }
+
+                $q->{$this->scope}($from, $to);
+            }
+
+        });
+    }
+
+    /////////////////////////////////
+    /// Parsing / casting
+    /////////////////////////////////
+
+    protected $input_format = 'Y-m-d';
+
+    public function inputFormat($format)
+    {
+        $this->input_format = $format;
+
+        return $this;
+    }
+
+    protected $cast_to_carbon = true;
+
+    public function castToCarbon($should_cast = true)
+    {
+        $this->cast_to_carbon = $should_cast;
+
+        return $this;
+    }
+
+    /////////////////////////////////
+    /// Exporting
+    /////////////////////////////////
+
+    protected function getExportType()
+    {
+        return 'Period';
+    }
+
+}
+
